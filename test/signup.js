5f5962af826f4a4763d3c738c159a7d8896ad34e
ChileanBoyFromPuenteAlto <gary.villegasb@gmail.com>
2024-02-24 13:53:25 -0300

    another changes
    this is going bad my boy

--- test/signup.js
+++ test/signup.js
@@ -12,3 +12,19 @@ const user = {
   password: document.getElementById("password"),
   repassword: document.getElementById("re-password"),
 };
+
+const form = document.getElementById("signupform");
+
+form.addEventListener("signup", (event) => {
+  if (user.email.valur !== user.reemail.value) {
+    alert("the email's doesn't match!");
+    event.preventDefault();
+    return;
+  }
+
+  if (user.password.value !== user.repassword.value) {
+    alert("the password's doesn't macth!");
+    event.preventDefault();
+    return;
+  }
+});
